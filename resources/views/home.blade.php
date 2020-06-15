f8b6fcb48685c73f1c2a78b34be38db957eab60d
Alessandro <alecritelli@gmail.com>
2020-06-16 00:14:09 +0200

    layout data and php complete
    (map & reduce to implement)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,49 @@
 
 @section ('content')
   <div class="content">
-    <p>Questo e' il contenuto!!!</p>
+    <h2>Pasta Lunga:</h2>
+    <ul>
+        @foreach ($lunga as $card)
+          <li>
+          <img src="{{ $card['src'] }}" alt="immagine formato pasta">
+          </li>
+          <li>
+          {{ $card['titolo'] }}
+          </li>
+          <li>
+          {{ $card['descrizione'] }}
+          </li>
+        @endforeach
+    </ul>
+
+    <h2>Pasta Corta:</h2>
+    <ul>
+        @foreach ($corta as $card)
+          <li>
+          <img src="{{ $card['src'] }}" alt="immagine formato pasta">
+          </li>
+          <li>
+          {{ $card['titolo'] }}
+          </li>
+          <li>
+          {{ $card['descrizione'] }}
+          </li>
+        @endforeach
+    </ul>
+
+    <h2>Pasta Cortissima:</h2>
+    <ul>
+        @foreach ($cortissima as $card)
+          <li>
+          <img src="{{ $card['src'] }}" alt="immagine formato pasta">
+          </li>
+          <li>
+          {{ $card['titolo'] }}
+          </li>
+          <li>
+          {{ $card['descrizione'] }}
+          </li>
+        @endforeach
+    </ul>
   </div>
 @endsection
